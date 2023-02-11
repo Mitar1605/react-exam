f12753e0b541f066eb92566772ad3364fca6b1a6
Mitar1605 <120776088+Mitar1605@users.noreply.github.com>
2023-02-11 23:13:18 +0400

    modified

--- src/pages/catalog/Catalog.js
+++ src/pages/catalog/Catalog.js
@@ -1,22 +1,98 @@
-import React from 'react'
+import React, { useEffect, useReducer, useRef, useState } from 'react'
 import './Catalog.css'
 import ProductBox from '../../components/product/ProductBox'
 import { productsData } from '../../assets/data/productsData'
+import {HiOutlineSearch} from 'react-icons/hi'
 
 export default function Catalog() {
 
+    const searchInputValue = useRef('')
+    
+    const initialProductState = {
+        isLoading: false,
+        isError: false,
+        data: []
+    }
+
+    const productReducerFunc = (state, action) => {
+        switch (action.type) {
+            case 'isLoading':
+                return {
+                    ...state,
+                    isLoading: true,
+                    isError: false
+                }
+            case 'isError':
+                return {
+                    ...state,
+                    isLoading: false,
+                    isError: true
+                }
+            case 'isSucces':
+                return {
+                    isLoading: false,
+                    isError: false,
+                    data: action.payload
+                }
+            }
+        }
+        
+    const [showData, setShowData] = useState([])
+    const [productState, dispatch] = useReducer(productReducerFunc ,initialProductState)
+    const {isLoading, isError, data} = productState
+    
+    useEffect(() => {
+        dispatch({type: 'isLoading'})
+        const getData = new Promise((resolve) => {
+            return resolve(productsData)
+        })
+        getData
+        .then(data => {
+            dispatch({type: 'isSucces', payload: data})
+            setShowData(data)
+        })
+        .catch(err => dispatch({type: 'isError'}))
+    }, [])
+    
+    function onSearch(inputValue) {
+        setShowData(data.filter(el => el.title.toLowerCase().includes(inputValue.current.toLowerCase())))
+    }
+    
+    
   return (
     <div className='catalog__main'>
-        <div className="catalog-products-block">
+        <div className="catalog-container">
+            <div className="catalog__title">
+                <h1>КАТАЛОГ ПРОДУКЦИИ</h1>
+            </div>
+            <div className="catalog-search-block">
+                <span>Фильтр</span>
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    onSearch(searchInputValue)
+                }}>
+                    <input type="text" placeholder='Поиск' onChange={(evt) => searchInputValue.current = evt.target.value} />
+                    <HiOutlineSearch onClick={() => onSearch(searchInputValue)}/>
+                </form>
+            </div>
+
             {
-                productsData.map(product => {
-                    return (
-                        <div className="catalog-product" key={product.id}>
-                            <ProductBox product={product} />
-                        </div>
-                    )
-                })
+                isLoading ? <div className="loader-div"><div className='loader'></div></div>: ''
             }
+            <div className="catalog-products-block">
+                {
+                    
+                    isError ? <h1>Ohh, Something went wrong!!!</h1>:
+                    showData.length === 0 ? <h1>Простите нечего не нашлось</h1>:
+                    showData.map(product => {
+                        return (
+                            <div className="catalog-product" key={product.id}>
+                                <ProductBox product={product} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     </div>
   )
