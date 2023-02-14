6906898044810bc03f6ed7ad5781a644e182923b
Mitar1605 <120776088+Mitar1605@users.noreply.github.com>
2023-02-14 20:01:58 +0400

    modified

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Header from './components/header/Header';
 import HomePage from './pages/homepage/HomePage';
 import Catalog from './pages/catalog/Catalog';
 import CatalogProductPage from './pages/catalog/catalog-product-page/CatalogProductPage';
+import CatalogKitchen from './pages/catalog/catalog-kitchen/CatalogKitchen';
+import ScrollToTop from './hooks/scrollToTop/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
+      <div className="hooks">
+        <ScrollToTop />
+      </div>
+
       <Header />
       
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/catalog' element={<Catalog />} />
+        <Route path='catalog-kitchen' element={<CatalogKitchen />} />
         <Route path='/catalog/:catalogEn/:productId/:title' element={<CatalogProductPage />} />
       </Routes>
     </div>
