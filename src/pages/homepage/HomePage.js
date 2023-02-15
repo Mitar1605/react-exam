8e009b2d4bff90d7f88ad4d900645984b1100db3
Mitar1605 <120776088+Mitar1605@users.noreply.github.com>
2023-02-15 22:14:38 +0400

    modified

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -20,6 +20,51 @@ export default function HomePage() {
           </div>
         </div>
       </div>
+
+      <div className="homepage-second-block">
+        <div className="second-block-container">
+          <div className="second-block second-block-1">
+            <h1>7</h1>
+            <p>Лет мы продаем товары для уюта вашего дома</p>
+          </div>
+          <div className="second-block second-block-1">
+            <h1>2 000</h1>
+            <p>Заказов ежемесячно обрабатывают менеджеры</p>
+          </div>
+          <div className="second-block second-block-1">
+            <h1>4 278</h1>
+            <p>Человек доверились нам</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="homepage-third-block">
+        <div className="third-block-container">
+          <div className="third-block-item third-block-item-1">
+            <img src={require('../../assets/images/homepagecard-logo.png')} a lt="logo" />
+          </div>
+          <div className="third-block-item third-block-item-2">
+            <img src={require('../../assets/images/homepagecard-logo.png')} a lt="logo" />
+          </div>
+          <div className="third-block-item third-block-item-3">
+            <img src={require('../../assets/images/homepagecard-logo.png')} a lt="logo" />
+          </div>
+          <div className="third-block-item third-block-item-4">
+            <img src={require('../../assets/images/homepagecard-logo.png')} a lt="logo" />
+          </div>
+        </div>
+      </div>
+
+      <div className="homepage-fourth-block">
+        <div className="third-block-container">
+          <div className="third-block-content">
+            <h1>ХОТИТЕ ПОЛУЧИТЬ СКИДОЧНЫЙ КУПОН НА ПЕРВУЮ ПОКУПКУ?</h1>
+            <p>Самыми лучшими способами приготовления еды являются следующие: варка на пару, запекание или варка в воде.</p>
+            <button className='button'>Получить купон</button>
+          </div>
+            <img src={require('../../assets/images/homepage-img-5.png')} alt="sale image" />
+        </div>
+      </div>
     </div>
   )
 }
